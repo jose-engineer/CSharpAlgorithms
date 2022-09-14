c585695dc1e40b3345e84eb138750e2af6ae0502
jose-engineer <jose-engineer@outlook.com>
2022-09-14 14:29:22 -0500

    Use generic List

--- Array_AggregateAndFilter/Program.cs
+++ Array_AggregateAndFilter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Array_AggregateAndFilter {
@@ -8,13 +9,13 @@ namespace Array_AggregateAndFilter {
             int[] arr1 = { -8, 2, 3, -9, 11, -20 };
             int[] arr2 = { 0, -2, -9, -39, 39, 10, 7 };
 
-            int[] evenArray = FindEvenNums(arr1, arr2);
+            int[] evenArray = FindEvenNums2(arr1, arr2);
 
             Array.ForEach(evenArray, Console.WriteLine);
         }
 
-        public static int[] FindEvenNums(int[] arr1, int[] arr2) {
-            ArrayList result = new ArrayList();
+        public static int[] FindEvenNums(int[] arr1, int[] arr2) {            
+            List<int> result = new List<int>();//ArrayList result = new ArrayList();
 
             foreach (var item in arr1) {
                 if (item % 2 == 0) {
@@ -27,15 +28,18 @@ namespace Array_AggregateAndFilter {
                     result.Add(item);
                 }
             }
-
-            return (int[])result.ToArray(typeof(int));
+            
+            return result.ToArray(); //return (int[])result.ToArray(typeof(int));
         }
 
-        //public static int[] FindEvenNums2(int[] arr1, int[] arr2) {            
-        //    int[] result1 = Array.FindAll(arr1, x => x % 2 == 0);
-        //    int[] result2 = Array.FindAll(arr2, x => x % 2 == 0);            
+        public static int[] FindEvenNums2(int[] arr1, int[] arr2) {
+            //int[] result1 = Array.FindAll(arr1, x => x % 2 == 0);
+            //int[] result2 = Array.FindAll(arr2, x => x % 2 == 0);                                    
 
-        //    return result1.Concat(result2).ToArray();
-        //}
+            IEnumerable<int> result1 = arr1.ToList().Where(x => x % 2 == 0);//List<int> result1 = arr1.ToList().FindAll(x => x % 2 == 0);
+            IEnumerable<int> result2 = arr2.ToList().Where(x => x % 2 == 0);//List<int> result2 = arr2.ToList().FindAll(x => x % 2 == 0);
+
+            return result1.Concat(result2).ToArray();
+        }
     }
 }
