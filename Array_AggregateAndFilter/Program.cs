d837f5473001dfd52d0909ac9592291e1c56f238
jose-engineer <jose-engineer@outlook.com>
2022-09-14 15:41:32 -0500

    Use Add Range

--- Array_AggregateAndFilter/Program.cs
+++ Array_AggregateAndFilter/Program.cs
@@ -9,7 +9,7 @@ namespace Array_AggregateAndFilter {
             int[] arr1 = { -8, 2, 3, -9, 11, -20 };
             int[] arr2 = { 0, -2, -9, -39, 39, 10, 7 };
 
-            int[] evenArray = FindEvenNums2(arr1, arr2);
+            int[] evenArray = FindEvenNums(arr1, arr2);
 
             Array.ForEach(evenArray, Console.WriteLine);
         }
@@ -32,14 +32,20 @@ namespace Array_AggregateAndFilter {
             return result.ToArray(); //return (int[])result.ToArray(typeof(int));
         }
 
-        public static int[] FindEvenNums2(int[] arr1, int[] arr2) {
-            //int[] result1 = Array.FindAll(arr1, x => x % 2 == 0);
-            //int[] result2 = Array.FindAll(arr2, x => x % 2 == 0);                                    
+        public static int[] FindEvenNums2(int[] arr1, int[] arr2) {                                                
+            List<int> result1 = arr1.ToList().Where(x => x % 2 == 0).ToList(); //List<int> result1 = arr1.ToList().FindAll(x => x % 2 == 0);
+            List<int> result2 = arr2.ToList().Where(x => x % 2 == 0).ToList(); //List<int> result2 = arr2.ToList().FindAll(x => x % 2 == 0);
+            
+            result1.AddRange(result2);
 
-            IEnumerable<int> result1 = arr1.ToList().Where(x => x % 2 == 0);//List<int> result1 = arr1.ToList().FindAll(x => x % 2 == 0);
-            IEnumerable<int> result2 = arr2.ToList().Where(x => x % 2 == 0);//List<int> result2 = arr2.ToList().FindAll(x => x % 2 == 0);
+            return result1.ToArray(); //return result1.Concat(result2).ToArray();            
+        }
 
-            return result1.Concat(result2).ToArray();
+        public static int[] FindEvenNums3(int[] arr1, int[] arr2) {
+            int[] result1 = Array.FindAll(arr1, x => x % 2 == 0);
+            int[] result2 = Array.FindAll(arr2, x => x % 2 == 0);                                                
+
+            return result1.Concat(result2).ToArray();            
         }
     }
 }
